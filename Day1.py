06217d318ea20f816d0cb779f5777997e48907eb
Theja120 <ravitheja7098@gmail.com>
2025-04-02 23:34:43 +0530

    day_1

---
+++ Day1.py
@@ -0,0 +1,19 @@
+# Integers , strings, Indexing , Slicing
+number1 = 2 #assigned a value to a variable
+number2 = 10 #assigned a value to a variable
+print(number1+number2) # added 2 variables and printed the output
+print(number1-number2) # subtracted 2 variables and printed the output
+print(number1*number2) # Multiplied 2 variables and printed the output
+print(number1/number2) # Divided 2 variables and here we obtained the o/p in decimal form
+print(number1//number2) # Divided 2 variables and here we obtained the o/p in integer form
+print(number1%number2) # Divided 2 variables and here we obtained the remainder 
+print(type(number2))  # Printed the type of the given variable
+print(type(number1)) # Printed the type of the given variable
+
+# strings
+print("Hello World") # Printed the First string
+print(len("Hello World")) # Printed the length of the string
+variable = "Hey Man" # assigned the string to the variable
+print(len(variable)) #Checked the length of the given string
+print(variable[6]) # Used the Index to point out the specific element 
+print(variable[:]) # Used the slicing  to select the all elements
